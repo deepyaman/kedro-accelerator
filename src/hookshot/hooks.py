08861e2490baf51cf6578b74d53171bf8f0738e1
Deepyaman Datta <deepyaman.datta@utexas.edu>
2020-06-08 09:03:30 -0500

    Also persist intermediate data sets asynchronously

--- src/hookshot/hooks.py
+++ src/hookshot/hooks.py
@@ -1,3 +1,6 @@
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from typing import Any, Dict
+
 from kedro.framework.hooks import hook_impl
 from kedro.io import DataCatalog, MemoryDataSet
 from kedro.pipeline import Pipeline
@@ -11,10 +14,44 @@ class TeePlugin:
 
         # Replace intermediate inputs and outputs with in-memory stores.
         # TODO(deepyaman): Identify the default data set for the runner.
+        self.data_set_names = set(catalog.list())
         catalog.add_all(
             {
                 name: MemoryDataSet()
-                for name in set(catalog.list()) - pipeline.inputs() - pipeline.outputs()
+                for name in self.data_set_names - pipeline.inputs() - pipeline.outputs()
             },
             replace=True,
         )
+
+        self.pool = ThreadPoolExecutor()
+        self.save_futures = set()
+
+    @hook_impl
+    def after_node_run(self, catalog: DataCatalog, outputs: Dict[str, Any]) -> None:
+        for name, data in outputs.items():
+            if name in self.data_set_names:  # ``DataSet`` is registered
+                physical_dataset = getattr(self.physical_catalog.datasets, name)
+
+                # If the intermediate data set was "replaced," persist a
+                # copy of the output data with the original save method.
+                if getattr(catalog.datasets, name) is not physical_dataset:
+                    self.save_futures.add(
+                        self.pool.submit(self.physical_catalog.save, name, data)
+                    )
+
+    def _cleanup(self):
+        try:
+            for future in as_completed(self.save_futures):
+                exception = future.exception()
+                if exception:
+                    raise exception
+        finally:
+            self.pool.shutdown(wait=True)
+
+    @hook_impl
+    def after_pipeline_run(self) -> None:
+        self._cleanup()
+
+    @hook_impl
+    def on_pipeline_error(self) -> None:
+        self._cleanup()
